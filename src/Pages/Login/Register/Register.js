93685fd3211ea8520829573e09b91cffeeab2ce6
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-04-15 18:05:45 +0600

    add to all and coplete to all

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from "../../../firebase.init"
 import SocialLogin from '../SocialLogin/SocialLogin';
-
-
 
 
 
@@ -25,21 +23,21 @@ const Register = () => {
       navigate('/login')
    }
 
-   if (user) {
-      navigate('/');
-   }
+   // if (user) {
+   //    navigate('/');
+   // }
 
-   const handleRegister = event => {
+   const handleRegister = async (event) => {
       event.preventDefault();
       const name = event.target.name.value;
       const email = event.target.email.value;
       const password = event.target.password.value;
       // const agree = event.target.terms.checked;
 
-      if (agree) {
-         createUserWithEmailAndPassword(email, password);
-      }
-
+      await createUserWithEmailAndPassword(email, password);
+      await updateProfile({ displayName: name });
+      alert('Updated profile');
+      navigate('/')
 
    }
 
